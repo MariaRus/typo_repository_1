51fe6de6c13f0f3945f09327e2ed612ae40f6bbd
Maryia Rusinovich <maryrusinovich@gmail.com>
2022-12-18 19:05:39 +0200

    commit test2

---
+++ test2.ts
@@ -0,0 +1,9 @@
+const family = [
+    {member: "mom", name: "Maryia", age: 37},
+    {member: "dad", name: "Andrei", age: 52},
+    //some comment
+    {member: "son", name: "Pavel", age: 7}
+]
+
+console.log("MY FAMILY: ")
+console.table (family);
